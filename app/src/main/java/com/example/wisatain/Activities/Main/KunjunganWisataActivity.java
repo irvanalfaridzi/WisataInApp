78b8de937d46acf938b9eb89872b825c563e0ba4
Alfaridzi <irvanalfaridzi@gmail.com>
2019-04-22 01:08:03 +0700

    belum_acc,sudah_acc,riwayat_kunjungan

---
+++ app/src/main/java/com/example/wisatain/Activities/Main/KunjunganWisataActivity.java
@@ -0,0 +1,15 @@
+package com.example.wisatain.Activities.Main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.wisatain.R;
+
+public class KunjunganWisataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kunjungan_wisata);
+    }
+}
