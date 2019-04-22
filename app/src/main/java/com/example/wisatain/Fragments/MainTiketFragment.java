1bd080d618808ee25f773ad9bb8755834bb6889e
Alfaridzi <irvanalfaridzi@gmail.com>
2019-04-22 11:55:04 +0700

    toket

--- app/src/main/java/com/example/wisatain/Fragments/MainTiketFragment.java
+++ app/src/main/java/com/example/wisatain/Fragments/MainTiketFragment.java
@@ -84,12 +84,8 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.CardView;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -98,20 +94,7 @@ import com.example.wisatain.Activities.Main.MainActivity;
 import com.example.wisatain.Activities.Main.TiketBelumDigunakanActivity;
 import com.example.wisatain.Activities.Main.TiketMenungguKonfirmasiActivity;
 import com.example.wisatain.Activities.Main.TiketTelahDigunakanActivity;
-import com.example.wisatain.Adapters.MainTiketAdapter;
-import com.example.wisatain.Items.Tiket;
 import com.example.wisatain.R;
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
-import com.firebase.ui.database.FirebaseRecyclerOptions;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
 import java.util.Objects;
 
 import butterknife.BindView;
@@ -154,6 +137,8 @@ public class MainTiketFragment extends Fragment {
         ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(false);
         ((MainActivity) getActivity()).getSupportActionBar().setDisplayShowHomeEnabled(false);
 
+        cvIntent();
+        
         return view;
     }
 
